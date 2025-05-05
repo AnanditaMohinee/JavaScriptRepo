487804dec66e1e26ef85045fa23fc9d5271700de
AnanditaMohinee <133890865+AnanditaMohinee@users.noreply.github.com>
2025-05-05 13:22:57 +0000

    variables Test done

---
+++ Jscode1/variables.js
@@ -0,0 +1,17 @@
+const accountID = 1234
+let accountEmail = "1234@gmil.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+/*
+accountID = 19876 // error- Assignment to constant variable.
+console.log(accountID);
+
+*/
+accountEmail = "hello@gmail.com"
+accountPassword = "12345678"
+accountCity = "Bangalore"
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
+
+//console.log(accountID);
